9819016fe4724e7400600dc80c01ddcd442ff719
Geoffrey <geoffrey.bernicot@gmail.com>
2019-02-17 13:11:10 +0100

    score calcul during the game and navigation to the final score

--- Components/Game.js
+++ Components/Game.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import { StyleSheet, View, Text, Button, ImageBackground, TouchableOpacity, TextInput } from 'react-native'
 import { randomPledge } from '../helpers/pledgeHelper'
+import { connect } from 'react-redux'
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 class Game extends React.Component {
@@ -32,8 +33,6 @@ class Game extends React.Component {
         else this.setState({ pledge: randomPledge(), currentPlayer: newCurrentPlayer, currentRound: newCurrentRound})
     }
 
-<<<<<<< Updated upstream
-=======
     _pledgeButton(){
         this.state.players[this.state.currentPlayer].totalPledge += this.state.pledge.powerPledge
         this.state.players[this.state.currentPlayer].totalPledgeOK += this.state.pledge.powerPledge
@@ -62,15 +61,10 @@ class Game extends React.Component {
         this._loadNewPledge()
     }
 
->>>>>>> Stashed changes
     render() {
         return (
             <ImageBackground source={require('../assets/images/background.jpeg')} style={{width: '100%', height: '100%'}}>
                 <View style={{ flex: 1, alignItems: 'center', marginTop: 50}}>
-<<<<<<< Updated upstream
-                    <Text style={{ borderRadius: 4, borderWidth: 2, borderColor: '#fff', backgroundColor: 'rgba(255, 255, 255, 0.5)', height: 100, width: 250 }}>Score de {this.state.players[this.state.currentPlayer].name}</Text>
-
-=======
                     <View style={{ padding: 10, borderRadius: 4, borderWidth: 2, borderColor: '#fff', backgroundColor: 'rgba(255, 255, 255, 0.5)', height: 100, width: 250 }}>
                         <Text style={{fontWeight: 'bold'}}>Score de {this.state.players[this.state.currentPlayer].name} :</Text>
                         <Text>Total de gage : {this.state.players[this.state.currentPlayer].totalPledge}</Text>
@@ -78,21 +72,23 @@ class Game extends React.Component {
                         <Text>Total de shot : {this.state.players[this.state.currentPlayer].totalDrink}</Text>
                         <Text>Total de shot BU : {this.state.players[this.state.currentPlayer].totalDrunk}</Text>
                     </View>
->>>>>>> Stashed changes
                     <View style={{marginTop: 50, borderRadius: 4, borderWidth: 2, borderColor: '#fff', backgroundColor: 'rgba(255, 255, 255, 0.5)', height: 420, width: 250 }}>
-                        <Text style={{ margin: 10 }}>{this.state.pledge.name} pour {this.state.players[this.state.currentPlayer].name}</Text>
+                        <View style={{height: 285}}>
+                            <Text style={{ margin: 10, fontWeight: 'bold',  }}>{this.state.pledge.name}</Text>
+                            <Text style={{ margin: 10, }}>{this.state.players[this.state.currentPlayer].name} {this.state.pledge.desc}</Text>
+                        </View>
                         <View style={{ flexDirection: 'row' }}>
-                            <TouchableOpacity onPress={() => this._loadNewPledge()}  style={{flexDirection: 'row', justifyContent:'center', alignItems: 'center', marginTop: 250, marginLeft: 10, borderRadius: 4, borderWidth: 2, borderColor: '#fff', width: 100, height: 50, backgroundColor: 'rgba(180, 127, 4, 0.5)'}} >
+                            <TouchableOpacity onPress={() => this._pledgeButton()}  style={{flexDirection: 'row', justifyContent:'center', alignItems: 'center', marginTop: 10, marginLeft: 10, borderRadius: 4, borderWidth: 2, borderColor: '#fff', width: 100, height: 50, backgroundColor: 'rgba(180, 127, 4, 0.5)'}} >
                                 <Icon name="reddit-alien" size={20} color={'white'} />
                                 <Text style={{color: '#fff'}}>Ca gage !</Text>
                             </TouchableOpacity>
 
-                            <TouchableOpacity onPress={() => this._loadNewPledge()}  style={{flexDirection: 'row', justifyContent:'center', alignItems: 'center', marginTop: 250, marginLeft: 25, borderRadius: 4, borderWidth: 2, borderColor: '#fff', width: 100, height: 50, backgroundColor: 'rgba(180, 89, 4, 0.5)'}} >
+                            <TouchableOpacity onPress={() => this._drinkButton()}  style={{flexDirection: 'row', justifyContent:'center', alignItems: 'center', marginTop: 10, marginLeft: 25, borderRadius: 4, borderWidth: 2, borderColor: '#fff', width: 100, height: 50, backgroundColor: 'rgba(180, 89, 4, 0.5)'}} >
                                 <Icon name="glass" size={20} color={'white'} />
                                 <Text style={{color: '#fff'}}>Je bois !</Text>
                             </TouchableOpacity>
                         </View>
-                            <TouchableOpacity onPress={() => this._loadNewPledge()}  style={{flexDirection: 'row', justifyContent:'center', alignItems: 'center', marginTop: 20, marginLeft: 80, borderRadius: 4, borderWidth: 2, borderColor: '#fff', width: 100, height: 50, backgroundColor: 'rgba(98, 4, 180, 0.5)'}} >
+                            <TouchableOpacity onPress={() => this._comboButton()}  style={{flexDirection: 'row', justifyContent:'center', alignItems: 'center', marginTop: 20, marginLeft: 80, borderRadius: 4, borderWidth: 2, borderColor: '#fff', width: 100, height: 50, backgroundColor: 'rgba(98, 4, 180, 0.5)'}} >
                                 <Icon name="superpowers" size={20} color={'white'} />
                                 <Text style={{color: '#fff'}}>Combo !!!</Text>
                         </TouchableOpacity>
@@ -103,4 +99,8 @@ class Game extends React.Component {
     }
 }
 
-export default Game
+const mapStateToProps = (state) => {
+    return state
+  }
+  
+  export default connect(mapStateToProps)(Game)
--- Navigation/Navigation.js
+++ Navigation/Navigation.js
@@ -2,6 +2,7 @@ import { createStackNavigator, createAppContainer, createSwitchNavigator } from 
 import Home from '../Components/Home'
 import Lobby from '../Components/Lobby'
 import Game from '../Components/Game'
+import Score from '../Components/Score'
 
 const SearchStackNavigator = createStackNavigator({
   Home: {
@@ -18,6 +19,12 @@ const SearchStackNavigator = createStackNavigator({
   },
   Game: {
     screen: Game,
+    navigationOptions: {
+      header: null
+    }
+  },
+  Score: {
+    screen: Score,
     navigationOptions: {
       header: null
     }
